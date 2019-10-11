3e5e43b9404812da0f688c46a018170e1aea54b4
Rehana Devani <rdevani@google.com>
2019-10-10 20:24:42 -0400

    almost working

--- integration_tests/tests/forseti/controls/scanner.rb
+++ integration_tests/tests/forseti/controls/scanner.rb
@@ -3,7 +3,6 @@ control 'Scanner' do
 
    describe yaml('/home/ubuntu/forseti-security/configs/forseti_conf_server.yaml') do
         before :context do
-            puts "Present working directory:", Dir.pwd
         end
 
         its(['inventory', 'root_resource_id']) { should match /organizations\/230198968274/ }
@@ -12,23 +11,22 @@ control 'Scanner' do
    describe "Run scanner" do
 
        before :context do
-           puts "Present working directory:", Dir.pwd
            command("forseti inventory purge 0").result
            command("forseti inventory create").result
            inventory_id = JSON.parse(command("forseti inventory list").stdout).fetch("id")
            command("forseti model create --inventory_index_id #{inventory_id} model_new").result
            command("forseti model use model_new").result
            unmodified_yaml = yaml('/home/ubuntu/forseti-security/configs/forseti_conf_server.yaml').params
-           modified_yaml = yaml('/home/ubuntu/forseti-security/configs/forseti_conf_server.yaml').params
-           modified_yaml["scanner"]["scanners"][1]["enable"] = "true"
-           command("echo #{modified_yaml} > /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml").result
-           # command("echo cat #{modified_yaml}")
-           command("head -10 #{modified_yaml}")
+           @modified_yaml = yaml('/home/ubuntu/forseti-security/configs/forseti_conf_server.yaml').params
+           @modified_yaml["scanner"]["scanners"][0]["enabled"] = "true"
+           puts "Audit logging Scanner", @modified_yaml["scanner"]["scanners"][0]["enabled"] = "true"
+           command("echo #{@modified_yaml} | tee /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml").result
+           puts "file content", command("cat /home/ubuntu/forseti-security/configs/forseti_conf_server.yaml").stdout
            command("forseti server configuration reload").result
        end
 
            it "should be visible from the command-line" do
-               expect(command("forseti scanner run").stdout).to match /Scan completed/
+               expect(command("forseti scanner run").stdout).to match /audit/
            end
 
 
